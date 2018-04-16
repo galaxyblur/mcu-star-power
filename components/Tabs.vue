f4ba1b338014039ee6cd0bbd36cfa6c87795cc81
Corky Brown <galaxyblur@gmail.com>
2018-04-16 19:01:47 -0400

    uk-tab

--- components/Tabs.vue
+++ components/Tabs.vue
@@ -2,7 +2,7 @@
   <div>
     <h5 class="uk-float-right uk-visible@m"><i>The most epic franchise in film history. The biggest movie ever.</i></h5>
     <h5 class="uk-hidden@m"><i>The most epic franchise in film history. The biggest movie ever.</i></h5>
-    <ul class="uk-flex-left" uk-tab>
+    <ul class="uk-flex-left uk-tab" uk-tab>
       <nuxt-link tag="li" to="/" active-class="uk-active" exact><a>Power Rankings</a></nuxt-link>
       <nuxt-link tag="li" to="/collector" active-class="uk-active"><a>For the Collector</a></nuxt-link>
       <nuxt-link tag="li" to="/grandmaster" active-class="uk-active"><a>For the Grandmaster</a></nuxt-link>
