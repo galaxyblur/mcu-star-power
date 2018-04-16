0f619c29a8dfb32700c4c1f90c55a1459259432e
Corky Brown <galaxyblur@gmail.com>
2018-04-16 17:49:16 -0400

    Header improvements

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="uk-text-center">
-    <h3 class="uk-h4 uk-margin-small uk-margin-top">The Stars of the</h3>
-    <h2 class="uk-h1 uk-margin-small">Marvel Cinematic Universe</h2>
-    <h1 class="uk-h5 uk-margin-small uk-text-meta">Power Rankings</h1>
+    <h3 class="uk-h4 uk-margin-small uk-margin-top">The Stars of</h3>
+    <h1 class="uk-margin-small">Avengers: Infinity War</h1>
+    <h2 class="uk-margin-small">and the Marvel Cinematic Universe</h2>
   </div>
 </template>
