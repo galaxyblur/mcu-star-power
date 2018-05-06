99e3e1391d35478121293b5eb65a7ec268fcdeb1
Corky Brown <galaxyblur@gmail.com>
2018-05-06 14:58:23 -0400

    Tab names update

--- components/Tabs.vue
+++ components/Tabs.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <b-nav tabs>
-      <b-nav-item to="/" exact>Power Rankings</b-nav-item>
-      <b-nav-item to="/collector">For the Collector</b-nav-item>
-      <b-nav-item to="/grandmaster">For the Grandmaster</b-nav-item>
+      <b-nav-item to="/" exact>MCU Power Rankings</b-nav-item>
+      <b-nav-item to="/collector">For the MCU Collector</b-nav-item>
+      <b-nav-item to="/grandmaster">For the MCU Grandmaster</b-nav-item>
       <b-nav-item to="/team-ups">Team-Ups</b-nav-item>
     </b-nav>
   </div>
